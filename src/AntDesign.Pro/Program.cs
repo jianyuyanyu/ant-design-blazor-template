7ae36acb1a17714eaeb0cc8e29b45a37bc9b6daf
jianyuyanyu <2416658234@qq.com>
2024-08-01 12:06:50 +0800

    Initial commit

---
+++ src/AntDesign.Pro/Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AntDesign.ProLayout;
+//#if (full)
+using AntDesign.Pro.Template.Services;
+//#endif
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AntDesign.Pro.Template
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//#if (host == 'wasm')
+            builder.RootComponents.Add<App>("#app");
+//#endif
+            builder.Services.AddScoped(
+                sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+
+            AddClientServices(builder.Services);
+
+            builder.Services.Configure<ProSettings>(builder.Configuration.GetSection("ProSettings"));
+
+            await builder.Build().RunAsync();
+        }
+
+        public static void AddClientServices(IServiceCollection services)
+        {
+            services.AddAntDesign();
+            //#if (full)
+            services.AddInteractiveStringLocalizer();
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services.AddScoped<IChartService, ChartService>();
+            services.AddScoped<IProjectService, ProjectService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<IProfileService, ProfileService>();
+            //#endif
+        }
+    }
+}
